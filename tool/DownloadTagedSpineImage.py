81eee9f0ca6a75816e092103d84dd3ab23abf70d
archer <1246506786@qq.com>
2016-08-11 21:13:40 +0800

    awesome

--- tool/DownloadTagedSpineImage.py
+++ tool/DownloadTagedSpineImage.py
@@ -16,9 +16,13 @@ sc = db['spinetmp']
 # foreach document, get isbn and img link
 imgs = sc.find({'isbn': {'$exists': True}})
 for img in imgs:
-    filename = img['isbn'] + '-' + img['url'].split('/')[-1]
-    response = unirest.get(img['url'])
-    with open('../data/img/' + filename, 'w') as F:
-        F.write(response.body)
+    try:
+        filename = img['isbn'] + '-' + img['url'].split('/')[-1]
+        unirest.timeout(20)
+        response = unirest.get(img['url'])
+        with open('../data/img/' + filename, 'w') as F:
+            F.write(response.body)
 
-    print img['isbn'], img['url']
+        print img['isbn'], img['url']
+    except:
+        print 'Error: ', img['_id']
