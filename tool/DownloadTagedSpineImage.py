2c4c05bc4d339e10964cfaf0180cedd7cd9c35b9
archer <1246506786@qq.com>
2016-08-11 21:02:57 +0800

    can download spine images

---
+++ tool/DownloadTagedSpineImage.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python
+# coding=utf-8
+#
+# Author: Archer Reilly
+# File: DownloadTagedSpineImage.py
+# Desc: 下载被标记的book spine image
+# Date: 11/Aug/2016
+# Produced By BR
+from pymongo import MongoClient
+import unirest
+
+client = MongoClient('mongodb://linyy:rioreader@192.168.200.22:27017/bookshelf')
+db = client['bookshelf']
+sc = db['spinetmp']
+
+# foreach document, get isbn and img link
+imgs = sc.find({'isbn': {'$exists': True}})
+for img in imgs:
+    filename = img['isbn'] + '-' + img['url'].split('/')[-1]
+    response = unirest.get(img['url'])
+    with open('../data/img/' + filename, 'w') as F:
+        F.write(response.body)
+
+    print img['isbn'], img['url']
