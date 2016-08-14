7a4e828bbe05c3e27778cac7d77a5af7ce6e0919
Archer Reilly <1246506786@qq.com>
2016-08-14 15:06:09 +0800

    surf not work on ../data/img/7801092465-1451141347736_spine.jpg

---
+++ tool/SURF.py
@@ -0,0 +1,13 @@
+import numpy as np
+import cv2
+from matplotlib import pyplot as plt
+
+img = cv2.imread('../data/img/7801092465-1451141347736_spine.jpg', 0)
+
+surf = cv2.SURF(300)
+
+kp, des = surf.detectAndCompute(img,None)
+
+img2 = cv2.drawKeypoints(img,kp,None,(255,0,0),4)
+
+plt.imshow(img2),plt.show()
